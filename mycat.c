2a94c1b025269058a02b402eb8f8ce39d117afd2
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-06-19 16:39:21 +0200

    implement mycat

--- mycat.c
+++ mycat.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	printf("bobo");
+	FILE *fp;
+	char ch;
+	char *file_name = argv[1];
+
+	//printf("%s", file_name);
+        fp = fopen(file_name, "r");
+
+	while((ch = fgetc(fp)) != EOF) {
+		printf("%c", ch);
+	}
 }
